b4f53c44fe43090fcd012d287d9bc7c4c9e277c3
Davide Plozza <dplozza@student.ethz.ch>
2021-12-18 18:25:48 +0100

    fixed quantization script for long paths

--- quantize.bat
+++ quantize.bat
@@ -1,4 +1,4 @@
 call activate ai8x-synthesis
-python ../ai8x-synthesis/quantize.py %1.pth.tar %1_q.pth.tar --device MAX78000 -v
+python ../ai8x-synthesis/quantize.py \\?\%1.pth.tar \\?\%1_q.pth.tar --device MAX78000 -v
 call activate ai8x-training
 
